59f93a6ea9e38b79683d029d8d2117ea64a84445
Luigi <luigifaldetta@gmail.com>
2023-06-19 14:48:39 +0200

    fix: update imports/exports

--- server/dist/controllers/msgController.js
+++ server/dist/controllers/msgController.js
@@ -11,36 +11,17 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.postMsg = exports.getMsg = void 0;
 const msg_1 = require("../model/msg");
-const getMsg = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    console.log("hel");
-    console.log(req.body);
-    //   console.log(req.query);
-    try {
-        // const { userId, correspondingUserId } = req.body;
-        const { fromUser, toUser } = req.body;
-        const query = {
-            $or: [
-                { fromUser, toUser },
-                { fromUser: toUser, toUser: fromUser },
-            ],
-        };
-        const foundMessages = yield msg_1.MessagesTin.find(query);
-        res.status(201).json({ message: "ok", data: foundMessages });
-    }
-    catch (error) {
-        res.status(500).send(error.message);
-        console.log(error);
-    }
-});
-exports.getMsg = getMsg;
-//this getMsg function works with my get request using params in my MessageService
 // export const getMsg = async (req: Request, res: Response) => {
+//   console.log("hel");
+//   console.log(req.body);
+//   //   console.log(req.query);
 //   try {
-//     const { userId, correspondingUserId } = req.query;
+//     // const { userId, correspondingUserId } = req.body;
+//     const { fromUser, toUser } = req.body;
 //     const query = {
 //       $or: [
-//         { fromUser: userId, toUser: correspondingUserId },
-//         { fromUser: correspondingUserId, toUser: userId },
+//         { fromUser, toUser },
+//         { fromUser: toUser, toUser: fromUser },
 //       ],
 //     };
 //     const foundMessages = await MessagesTin.find(query);
@@ -50,14 +31,47 @@ exports.getMsg = getMsg;
 //     console.log(error);
 //   }
 // };
+//this getMsg function works with my get request using params in my MessageService
+const getMsg = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { userId, correspondingUserId } = req.query;
+        console.log(req.query);
+        const query = { fromUser: userId, toUser: correspondingUserId };
+        const foundMessages = yield msg_1.MessagesTin.find(query);
+        console.log(foundMessages);
+        res.status(201).json({ message: "ok", data: foundMessages });
+    }
+    catch (error) {
+        res.status(500).send(error.message);
+        console.log(error);
+    }
+});
+exports.getMsg = getMsg;
 // // Add a Message to our Database
+// export const postMsg = async (req: Request, res: Response) => {
+//   try {
+//     const message = req.body;
+//     const insertedMessage = await MessagesTin.create(message);
+//     console.log("All good!");
+//     return res.status(201).json({ message: "ok", data: insertedMessage });
+//     // return insertedMessage;
+//   } catch (error: any) {
+//     console.log("Post message failed with: ", error.message);
+//     res.status(500).send(error.message);
+//   }
+// };
 const postMsg = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const message = req.body;
-        const insertedMessage = yield msg_1.MessagesTin.create(message);
+        const { message, userId, correspondingUserId } = req.body;
+        // console.log(req.body);
+        const insertedMessage = yield msg_1.MessagesTin.create({
+            message: message,
+            fromUser: userId,
+            toUser: correspondingUserId,
+        });
+        // console.log(insertedMessage);
         console.log("All good!");
         return res.status(201).json({ message: "ok", data: insertedMessage });
-        // return insertedMessage;
     }
     catch (error) {
         console.log("Post message failed with: ", error.message);
--- server/src/controllers/msgController.ts
+++ server/src/controllers/msgController.ts
@@ -1,39 +1,18 @@
 import { Response, Request } from "express";
 import { MessagesTin } from "../model/msg";
 
-export const getMsg = async (req: Request, res: Response) => {
-  console.log("hel");
-  console.log(req.body);
-  //   console.log(req.query);
-  try {
-    // const { userId, correspondingUserId } = req.body;
-    const { fromUser, toUser } = req.body;
-
-    const query = {
-      $or: [
-        { fromUser, toUser },
-        { fromUser: toUser, toUser: fromUser },
-      ],
-    };
-    const foundMessages = await MessagesTin.find(query);
-
-    res.status(201).json({ message: "ok", data: foundMessages });
-  } catch (error: any) {
-    res.status(500).send(error.message);
-    console.log(error);
-  }
-};
-
-//this getMsg function works with my get request using params in my MessageService
-
 // export const getMsg = async (req: Request, res: Response) => {
+//   console.log("hel");
+//   console.log(req.body);
+//   //   console.log(req.query);
 //   try {
-//     const { userId, correspondingUserId } = req.query;
+//     // const { userId, correspondingUserId } = req.body;
+//     const { fromUser, toUser } = req.body;
 
 //     const query = {
 //       $or: [
-//         { fromUser: userId, toUser: correspondingUserId },
-//         { fromUser: correspondingUserId, toUser: userId },
+//         { fromUser, toUser },
+//         { fromUser: toUser, toUser: fromUser },
 //       ],
 //     };
 //     const foundMessages = await MessagesTin.find(query);
@@ -45,14 +24,49 @@ export const getMsg = async (req: Request, res: Response) => {
 //   }
 // };
 
+//this getMsg function works with my get request using params in my MessageService
+
+export const getMsg = async (req: Request, res: Response) => {
+  try {
+    const { userId, correspondingUserId } = req.query;
+    console.log(req.query);
+
+    const query = { fromUser: userId, toUser: correspondingUserId };
+
+    const foundMessages = await MessagesTin.find(query);
+    console.log(foundMessages);
+    res.status(201).json({ message: "ok", data: foundMessages });
+  } catch (error: any) {
+    res.status(500).send(error.message);
+    console.log(error);
+  }
+};
+
 // // Add a Message to our Database
+// export const postMsg = async (req: Request, res: Response) => {
+//   try {
+//     const message = req.body;
+//     const insertedMessage = await MessagesTin.create(message);
+//     console.log("All good!");
+//     return res.status(201).json({ message: "ok", data: insertedMessage });
+//     // return insertedMessage;
+//   } catch (error: any) {
+//     console.log("Post message failed with: ", error.message);
+//     res.status(500).send(error.message);
+//   }
+// };
 export const postMsg = async (req: Request, res: Response) => {
   try {
-    const message = req.body;
-    const insertedMessage = await MessagesTin.create(message);
+    const { message, userId, correspondingUserId } = req.body;
+    // console.log(req.body);
+    const insertedMessage = await MessagesTin.create({
+      message: message,
+      fromUser: userId,
+      toUser: correspondingUserId,
+    });
+    // console.log(insertedMessage);
     console.log("All good!");
     return res.status(201).json({ message: "ok", data: insertedMessage });
-    // return insertedMessage;
   } catch (error: any) {
     console.log("Post message failed with: ", error.message);
     res.status(500).send(error.message);
