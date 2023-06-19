72131da0e81b03bfa513234047325f9acd303d0d
Luigi <luigifaldetta@gmail.com>
2023-06-19 14:39:29 +0200

    feat: successfully showing posted messages

---
+++ client/src/Services/MessageService.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+const MessageService = {
+  getMsg: async (userOneId: string, userTwoId: string) => {
+    try {
+      const response = await axios.get("http://localhost:3000/messages", {
+        params: { userId: userOneId, correspondingUserId: userTwoId },
+      });
+      console.log(response.data.data);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  postMsg: async (message: string, userOneId: string, userTwoId: string) => {
+    try {
+      const response = await axios.post(`http://localhost:3000/message`, {
+        message: message,
+        userId: userOneId,
+        correspondingUserId: userTwoId,
+      });
+      //   console.log(response);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  },
+};
+
+export default MessageService;
---
+++ client/src/Services/UserService.ts
@@ -0,0 +1,70 @@
+import axios from "axios";
+
+const UserService = {
+  getUsers: () => {
+    try {
+      const response = axios.get(`http://localhost:3000/users`);
+      return response;
+    } catch (error) {
+      throw error;
+    }
+  },
+  postUser: async (email: string, password: string, isSignUp: boolean) => {
+    try {
+      const response = await axios.post(
+        `http://localhost:3000/${isSignUp ? "signup" : "login"}`,
+        {
+          email,
+          password,
+        }
+      );
+
+      return response;
+    } catch (error) {
+      throw error;
+    }
+  },
+  updateUser: async (data: object, avatar: string, userId: string) => {
+    try {
+      const response = await axios.put(`http://localhost:3000/updateUser`, {
+        ...data,
+        avatar,
+      });
+      return response;
+    } catch (error) {
+      throw error;
+    }
+  },
+  getUser: async (userId: string) => {
+    try {
+      const response = await axios.get(`http://localhost:3000/user/${userId}`);
+      return response;
+    } catch (error) {
+      throw error;
+    }
+  },
+  updateMatch: async (userId: string, matchedUserId: string) => {
+    try {
+      console.log(userId);
+      await axios.put("http://localhost:3000/addmatch", {
+        userId,
+        matchedUserId,
+      });
+      axios.get(`http://localhost:3000/user/${userId}`);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  getMatchedUsers: async (matchedUserIds: Array<string>) => {
+    try {
+      const response = await axios.get("http://localhost:3000/matchedusers", {
+        params: { userIds: JSON.stringify(matchedUserIds) },
+      });
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
+
+export default UserService;
